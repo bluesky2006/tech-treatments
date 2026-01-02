cdd6017c5eba9e49e964f7411bf9729f03857f8e
bluesky2006 <simon.busby@gmail.com>
2026-01-02 13:46:39 +0000

    tweak white colour to off-white zinc

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -7,10 +7,9 @@ export default function SiteFooter() {
         <div className="flex flex-wrap items-center justify-between gap-4">
           <p>© {new Date().getFullYear()} Tech Treatments. Frome, Somerset.</p>
           <p className="flex flex-wrap gap-2">
-            <a className="hover:text-white" href="mailto:techtreatments@gmail.com">
+            <a className="hover:text-zinc-100" href="mailto:techtreatments@gmail.com">
               techtreatments@gmail.com
             </a>
-           
           </p>
         </div>
       </Container>
